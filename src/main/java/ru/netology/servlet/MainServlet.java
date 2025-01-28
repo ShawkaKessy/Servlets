04ce93beef5bd506a2b234e37a86905e026fb46c
ShawkaKessy <romarekonshin@gmail.com>
2025-01-28 22:00:38 +0300

    java DI

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -6,7 +6,6 @@ import ru.netology.service.PostService;
 import ru.netology.repository.PostRepository;
 import ru.netology.config.AppConfig;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,23 +15,20 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() throws ServletException {
-    // Инициализация контекста Spring
     AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
     context.register(AppConfig.class);
     context.refresh();
 
-    // Получаем бины из Spring контекста
     PostRepository postRepository = context.getBean(PostRepository.class);
     PostService postService = new PostService(postRepository);
     PostController controller = new PostController(postService);
 
-    // Устанавливаем контроллер
     getServletContext().setAttribute("controller", controller);
   }
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, java.io.IOException {
     PostController controller = (PostController) getServletContext().getAttribute("controller");
-    // Логика обработки запроса
   }
 }
