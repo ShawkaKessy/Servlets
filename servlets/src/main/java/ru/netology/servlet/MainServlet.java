f8e7f9c81b51414f8e3885ac23ebb02d70858bec
Shawkakessy <142739905+ShawkaKessy@users.noreply.github.com>
2025-01-28 20:47:51 +0300

    Update MainServlet.java

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,55 +1,39 @@
 package ru.netology.servlet;
 
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
+import ru.netology.repository.PostRepository;
+import ru.netology.config.AppConfig;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
 
   @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+  public void init() throws ServletException {
+    // Инициализация контекста Spring
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(AppConfig.class);
+    context.refresh();
+
+    // Получаем бины из Spring контекста
+    PostRepository postRepository = context.getBean(PostRepository.class);
+    PostService postService = new PostService(postRepository);
+    PostController controller = new PostController(postService);
+
+    // Устанавливаем контроллер
+    getServletContext().setAttribute("controller", controller);
   }
 
   @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    }
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, java.io.IOException {
+    PostController controller = (PostController) getServletContext().getAttribute("controller");
+    // Логика обработки запроса
   }
 }
 
