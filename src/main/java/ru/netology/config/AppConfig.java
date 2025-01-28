1583916a5854adf69e2ed6c7d2ba71a2769bdc9f
Shawkakessy <142739905+ShawkaKessy@users.noreply.github.com>
2025-01-28 22:13:51 +0300

    Update AppConfig.java

--- src/main/java/ru/netology/config/AppConfig.java
+++ src/main/java/ru/netology/config/AppConfig.java
@@ -1,26 +1,11 @@
 package ru.netology.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@ComponentScan(basePackages = "ru.netology")
+@EnableWebMvc
 public class AppConfig {
-
-    @Bean
-    public PostRepository postRepository() {
-        return new PostRepository();
-    }
-
-    @Bean
-    public PostService postService() {
-        return new PostService(postRepository());
-    }
-
-    @Bean
-    public PostController postController() {
-        return new PostController(postService());
-    }
 }
